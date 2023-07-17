b6f8d2095b8d4ebec2caa51b7e1e5058e519d620
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-07-17 18:36:38 +0800

    Update OpenAIClient.js
    fix

--- api/app/clients/OpenAIClient.js
+++ api/app/clients/OpenAIClient.js
@@ -103,7 +103,7 @@ class OpenAIClient extends BaseClient {
 
     if (this.options.reverseProxyUrl) {
       this.completionsUrl = this.options.reverseProxyUrl;
-      if (!model.startsWith('gpt-3.5')||model.endsWith('-poe')) {
+      if (!this.modelOptions.model.startsWith('gpt-3.5')||this.modelOptions.model.endsWith('-poe')) {
         this.completionsUrl = process.env.POE_OPENAI_REVERSE_PROXY;
       }      
     } else if (isChatGptModel) {
