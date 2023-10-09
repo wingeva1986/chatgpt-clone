fc4b6d21aefd2adb6379909551f1580c5317915d
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-10-09 11:17:55 +0800

    Revert "feat(localization): add Traditional Chinese language support (#1006)"
    This reverts commit 3137f467a89698ef4de46eda77f13f97fe7ea40b.

--- client/src/localization/Translation.tsx
+++ client/src/localization/Translation.tsx
@@ -10,7 +10,6 @@ import Russian from './languages/Ru';
 import Japanese from './languages/Jp';
 import Swedish from './languages/Sv';
 import Korean from './languages/Ko';
-import TraditionalChinese from './languages/ZhTraditional';
 // === import additional language files here === //
 
 const languageMap: { [key: string]: unknown } = {
@@ -26,7 +25,6 @@ const languageMap: { [key: string]: unknown } = {
   'ja-JP': Japanese,
   'sv-SE': Swedish,
   'ko-KR': Korean,
-  'zh-TC': TraditionalChinese,
   // Add additional language mappings here
 };
 
